c18d9288d12c9858faf257b0f0f1928c8798cf04
centerwebpro <102520204+centerwebpro@users.noreply.github.com>
2022-04-14 11:58:35 +0500

    4/shimiyo-menu Improved js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,9 +22,26 @@ function readJson() {
 
 readJson()
 
+
+
 // We enable .scrollline-text when full load page
 window.addEventListener('load', (event) => {
     var elements = document.querySelectorAll('.scrollline-text');
     for (var i = 0; i < elements.length; i++)
     elements[i].style.display = 'block';
+});
+
+
+
+// Menu header fullscreen
+let popup = document.querySelector('.popup');
+let header = document.querySelector('.top');
+let hamburger = document.querySelector('.main-left__hamburger-image');
+let cross = document.querySelector('.main-left__hamburger-imagecross');
+let btn = document.querySelector('.main-left__logo-hamburger');
+btn.addEventListener('click', function (event) {
+    popup.classList.toggle('popup_enable');
+    header.classList.toggle('popup-white');
+    hamburger.classList.toggle('popup_disable');
+    cross.classList.toggle('popup_enable');
 });
